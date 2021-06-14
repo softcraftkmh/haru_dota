ad843af31b617a99112d74e3b163a00bc21f0dd5
Kaung Myat Hein <softcraftkmh@gmail.com>
2021-06-14 11:46:12 +0630

    feat: getStaticProps for Home Page

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,9 +4,16 @@ import AppSEO from 'components/shared/SEO';
 import Matches from 'components/Matches';
 
 import { Match } from 'models/data';
+import { GetStaticProps } from 'next';
 
-const index = () => {
-	const { data } = useSWR<Match[]>('/proMatches');
+type indexProps = {
+	matches: Match[];
+};
+
+const index: React.FC<indexProps> = ({ matches }) => {
+	const { data } = useSWR<Match[]>('/proMatches', null, {
+		initialData: matches,
+	});
 
 	return (
 		<>
@@ -14,6 +21,13 @@ const index = () => {
 			{data && <Matches matches={data} />}
 		</>
 	);
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+	const matches = await await (
+		await fetch(`${process.env.NEXT_PUBLIC_OPEN_DOTA_API}/proMatches`)
+	).json();
+	return { props: { matches } };
 };
 
 export default index;
